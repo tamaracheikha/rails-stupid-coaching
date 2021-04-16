47bdfe76cec83c3ec5880deef89be4e2e95c4792
Tamara Cheikha <tamarasihamcheikha@gmail.com>
2021-04-16 11:41:58 +0100

    few changes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,9 +4,10 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-    if @question == "I am going to work"
+
+    if @question == 'I am going to work'
       @answer = 'Great!'
-    elsif @question.end_with?("?")
+    elsif @question.end_with?('?')
       @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = "I don't care, get dressed and go to work!"
